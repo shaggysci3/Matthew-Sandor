69ada8d8828f69ba599bf67e221366b8432ae0e2
matthew <shaggysci3@gmail.com>
2024-02-18 01:39:05 -0500

    whole hammyMenu

--- mywebpage/src/components/Navbar.jsx
+++ mywebpage/src/components/Navbar.jsx
@@ -15,15 +15,34 @@ const Navbar = () => {
 
          
             <div className='topBar'>
+              
               <div className='Logo'>
-              <Link to="/" className="MenuItem" >
-                Home
+              <div></div>
+              <div>
+              <Link to="/" className="MenuItem" style={{position: 'relative',top:'2rem',left:'0.3rem'}}>
+                &nbsp;Home
               </Link>
 
 
-              <Link to="/contacts" className="MenuItem">
-                  Contact Me
+              <Link to="/contacts" className="MenuItem2" style={{position: 'relative',top:'1.1rem',left:'-0.5rem'}}>
+              &nbsp;contacT&nbsp;
               </Link>
+
+              <Link to="/git" className="MenuItem" style={{position: 'relative',top:'1rem',left:'0.3rem'}}>
+              &nbsp;About 
+              </Link> 
+
+              <Link to="/contacts" className="MenuItem" style={{position: 'relative',top:'-1rem',left:'0.3rem'}}>
+              &nbsp;Contact&nbsp;
+              </Link>
+
+              <Link to="/contacts" className="MenuItem2" style={{position: 'relative',top:'-1.9rem',left:'-0.5rem'}}>
+              &nbsp;cOntact 
+              </Link>
+
+              
+              </div>
+              
               </div>
               {/* <img className='Logo' src='src/assets/pixil-frame-0.png'>
               </img> */}
