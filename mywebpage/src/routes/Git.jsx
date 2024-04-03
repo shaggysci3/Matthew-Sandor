64b57a45f0c942f72794e17957fef9d9fa3480e0
matthew <shaggysci3@gmail.com>
2024-04-03 07:06:59 -0400

    external img storage cuz local does not work for some reason

--- mywebpage/src/routes/Git.jsx
+++ mywebpage/src/routes/Git.jsx
@@ -20,7 +20,7 @@ const Git = () =>{
 
         <h1>Time Clock</h1>
         <div className="projectContainer">
-        <img className="displayImg" src="src/assets/TimeClock (2).png"></img>
+        <img className="displayImg" src="https://cdn.pixilart.com/photos/large/1856ff3188846e3.png"></img>
           <div className="projectInfo">
             <h1>Title</h1>
             <a href="https://github.com/AustinBByrd/Phase-3-project" target="_blank" rel="noopener noreferrer">time clock app</a>
@@ -30,7 +30,7 @@ const Git = () =>{
         
         <h1>Coffee Shop</h1>
         <div className="projectContainer">
-        <img className="displayImg" src="src/assets/Coffee.png"></img>
+        <img className="displayImg" src="https://cdn.pixilart.com/photos/large/5e0381b11831b59.png"></img>
         <div className="projectInfo">
             <h1>Title</h1>
             <a href="https://github.com/shaggysci3/coffee" target="_blank" rel="noopener noreferrer">time clock app</a>
